b3c10555433d51d0057dbe40bc6b62145e5bbf0e
Guilherme Tarifa Vieira <guilherme.vieira@sptech.school>
2023-06-16 13:47:50 -0300

    deploy

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,25 @@
 import React, { useEffect, useState } from "react";
 import PokemonThumb from "./components/PokemonThumb";
-import { SiPokemon } from 'react-icons/si';
-
+import { SiPokemon } from "react-icons/si";
 
 function App() {
   const [allPokemon, setAllPokemon] = useState([]);
   const [loadedPokemonCount, setLoadedPokemonCount] = useState(0);
-  const [loadMore, setLoadMore] = useState(`https://pokeapi.co/api/v2/pokemon?limit=20&offset=${loadedPokemonCount}`);
+  const [loadMore, setLoadMore] = useState(
+    `https://pokeapi.co/api/v2/pokemon?limit=20&offset=${loadedPokemonCount}`
+  );
 
-  const [noOffSet, setOffSet] = useState("https://pokeapi.co/api/v2/pokemon?limit=20");
+  const [noOffSet, setOffSet] = useState(
+    "https://pokeapi.co/api/v2/pokemon?limit=20"
+  );
+
+
+  const getPokemonInfo = async (id) => {
+    const res = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`);
+    const data = await res.json();
+
+    console.log(data);
+  };
 
   const getAllPokemons = async () => {
     const res = await fetch(loadedPokemonCount > 0 ? loadMore : noOffSet);
@@ -16,7 +27,7 @@ function App() {
 
     setLoadMore(data.next);
 
-    const results = data.results.sort((a, b) => a.id - b.id); // Ordena resultados por ID
+    const results = data.results.sort((a, b) => a.id - b.id);
 
     async function createPokeObj(results) {
       const pokeData = await Promise.all(
@@ -29,7 +40,7 @@ function App() {
 
       setAllPokemon((currentList) => {
         const filteredList = pokeData.filter(({ id }) => {
-          return !currentList.find(pokemon => pokemon.id === id);
+          return !currentList.find((pokemon) => pokemon.id === id);
         });
         return [...currentList, ...filteredList];
       });
@@ -45,7 +56,7 @@ function App() {
   const handleLoadMoreClick = () => {
     setLoadedPokemonCount(loadedPokemonCount + 20);
 
-    console.log(loadedPokemonCount)
+    console.log(loadedPokemonCount);
   };
 
   return (
@@ -53,25 +64,27 @@ function App() {
       <div className="app-container">
         <div className="display">
           <SiPokemon className="poke" size={200} />
-
         </div>
         <div className="pokemon-container">
           <div className="all-container">
-            {allPokemon.slice(0, loadedPokemonCount + 20).map(pokemon => (
+            {allPokemon.slice(0, loadedPokemonCount + 20).map((pokemon) => (
               <PokemonThumb
                 id={pokemon.id}
                 name={pokemon.name}
                 image={pokemon.sprites.other.dream_world.front_default}
                 type={pokemon.types[0].type.name}
                 key={pokemon.id}
+                onClickFunction={() => getPokemonInfo(pokemon.id)}
               />
             ))}
           </div>
-          <button className="load-more" onClick={handleLoadMoreClick}>Load more</button>
+          <button className="load-more" onClick={handleLoadMoreClick}>
+            Load more
+          </button>
         </div>
       </div>
     </>
-  )
+  );
 }
 
 export default App;
--- src/components/PokemonThumb.jsx
+++ src/components/PokemonThumb.jsx
@@ -1,26 +1,25 @@
-import React from 'react'
+import React from "react";
 
-const PokemonThumb = ({ id, name, image, type }) => {
-  const style = `thumb-container ${type}`
-
+const PokemonThumb = ({ id, name, image, type, onClickFunction }) => {
+  const style = `thumb-container ${type}`;
 
   return (
     <>
-      <div className={style}>
-
+      <div className={style} onClick={onClickFunction}>
         <div className="number">
-          <small>#0{id}</small>
+          <small>
+            {id < 100 ? "0" : ""}
+            {id}
+          </small>
         </div>
         <img src={image} alt={name} />
-        <div className='detail-wrapper'>
+        <div className="detail-wrapper">
           <h3>{name}</h3>
           <small>Type: {type}</small>
         </div>
       </div>
-
-
     </>
-  )
-}
+  );
+};
 
-export default PokemonThumb
+export default PokemonThumb;
